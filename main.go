1676e242d9fbb148fe2e86b9ff185d61aae2f52b
Anthony-4926 <2936616693@qq.com>
2023-03-01 20:58:59 +0800

    探讨上下文

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 func main() {
-	var a uint8 = 255
-	var b uint8 = 1
-	fmt.Printf("%d的二进制: %08b\n", a, a)
-	fmt.Printf("  %d的二进制: %08b\n", b, b)
-	fmt.Printf("a+b的二进制: %08b\n", a+b)
+	ctx := context.Background()
+	ctx, _ = context.WithDeadline(ctx, time.Now().Add(time.Second))
+	ctx.Done()
+
+	fmt.Println(ctx.Deadline())
 }
